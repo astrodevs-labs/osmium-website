4667568f557c141ed6254eddbd4939dc65a94766
leon3108 <maxime.noel@epitech.eu>
2024-02-23 21:22:45 +0000

    Add sparkles effect on hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { FaGithub } from 'react-icons/fa'
 import { Button } from './ui/button'
+import { SparklesCore } from './ui/sparkles'
 
 export default function Hero() {
   const github = 'https://github.com/astrodevs-labs/osmium'
@@ -10,23 +13,34 @@ export default function Hero() {
       className="my-40 flex flex-col items-center justify-center space-y-8"
       id="home"
     >
-      <p className="text-6xl font-bold text-osmium">Osmium</p>
-      <p className="text-3xl">
-        <strong>Develop</strong>, <strong>Build</strong> and{' '}
-        <strong>Deploy</strong> smart contracts easily{' '}
-      </p>
-      <div className="flex">
-        <Button className="mr-2 border-4 border-solid border-osmium bg-osmium hover:bg-background hover:text-osmium ">
-          <a href={marketplace} target="_blank">
-            View marketplace
-          </a>
-        </Button>
-        <Button>
-          <a href={github} target="_blank" className="flex items-center">
-            <FaGithub className="mr-2" />
-            Github
-          </a>
-        </Button>
+      <div className="relative flex h-[40rem] w-full flex-col items-center justify-center overflow-hidden rounded-md bg-black">
+        <p className="z-20 text-6xl font-bold text-osmium">Osmium</p>
+        <p className="z-20 text-3xl">
+          <strong>Develop</strong>, <strong>Build</strong> and{' '}
+          <strong>Deploy</strong> smart contracts easily{' '}
+        </p>
+        <div className="z-20 flex">
+          <Button className="mr-2 border-4 border-solid border-osmium bg-osmium hover:bg-background hover:text-osmium ">
+            <a href={marketplace} target="_blank">
+              View marketplace
+            </a>
+          </Button>
+          <Button>
+            <a href={github} target="_blank" className="flex items-center">
+              <FaGithub className="mr-2" />
+              Github
+            </a>
+          </Button>
+        </div>
+        <div className="absolute inset-0 h-screen w-full">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            className="h-full w-full bg-background"
+            background="bg-white"
+            particleDensity={30}
+            particleColor="#3C9DA1"
+          />
+        </div>
       </div>
     </section>
   )
