6c774d62b6947d17104880f57353cb35e31cd915
leon3108 <maxime.noel@epitech.eu>
2024-03-08 19:37:56 +0000

    Update About component styling and card width

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -4,19 +4,19 @@ import { Card, CardContent, CardHeader } from './ui/card'
 export default function About() {
   return (
     <section className="mx-4 my-32 space-y-52 md:px-8 lg:px-16" id="about">
-      <div className="flex justify-center">
+      <div className="flex justify-center space-x-2">
         <Image
           src="/icon-transparent.png"
           alt="osmium logo"
           width={300}
           height={300}
-          className="hidden lg:block lg:h-44 lg:w-44"
+          className="hidden rounded-lg lg:block lg:h-96 lg:w-96"
         />
-        <Card>
+        <Card className="2xl:w-[500px]">
           <CardHeader className="text-2xl font-bold">
             Current problem in smart contract developpement
           </CardHeader>
-          <CardContent className="w-full text-xl lg:w-[700px] 2xl:w-[900px]">
+          <CardContent className="w-full text-xl lg:w-[400px] xl:w-full">
             <p className="break-words">
               This project originated from a straightforward realization: the
               development of a smart contract demands the utilization of
@@ -34,7 +34,7 @@ export default function About() {
           <CardHeader className="text-pretty text-2xl font-bold">
             What is Osmium and how does it solve the problem ?
           </CardHeader>
-          <CardContent className="w-full text-xl lg:w-[400px]">
+          <CardContent className="w-full text-xl lg:w-[400px] xl:w-full">
             <p className="break-words">
               The answer lies in a comprehensive VS Code extension designed to
               streamline smart contract development. Offering a suite of
