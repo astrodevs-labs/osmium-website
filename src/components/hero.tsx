0d6f6730a8da358527639fb8d34ee5a6b2399f95
leon3108 <maxime.noel@epitech.eu>
2024-06-17 09:49:16 +0200

    align text in hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,9 +5,6 @@ import Link from 'next/link'
 import { buttonVariants } from './ui/button'
 
 export default function Hero() {
-  const github = 'https://github.com/astrodevs-labs/osmium'
-  const marketplace =
-    'https://marketplace.visualstudio.com/items?itemName=OsmiumToolchains.osmium-solidity-extension'
   return (
     <section
       className="flex flex-col items-center justify-center space-y-8 py-24"
@@ -17,12 +14,12 @@ export default function Hero() {
         <p className="z-10 text-5xl font-bold text-osmium sm:text-8xl">
           Osmium
         </p>
-        <p className="z-10 text-3xl sm:px-4 sm:text-5xl">
+        <p className="z-10 text-3xl sm:text-5xl">
           <strong>Develop</strong>, <strong>Build</strong> and
           <br />
           <strong>Deploy</strong> smart contracts easily
         </p>
-        <div className="z-10 mt-10 flex ">
+        <div className="z-10 mt-10 flex">
           <Link
             href="#about"
             className={buttonVariants({ variant: 'default' })}
