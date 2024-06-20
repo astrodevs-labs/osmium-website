1ba1ef1c4b91005a84992bed7a0d7286606d7b34
leon3108 <maxime.noel@epitech.eu>
2024-06-20 09:09:37 +0200

    feat: responsive Hero Section

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -5,10 +5,10 @@ import Link from 'next/link'
 export default function Hero() {
   return (
     <section
-      className="flex flex-col items-center justify-center space-y-8 py-24"
+      className="mb-16 flex flex-col items-center justify-center space-y-8 py-24 2xl:mb-48"
       id="home"
     >
-      <div className="relative flex h-[40rem] w-full flex-col justify-center overflow-hidden rounded-md bg-black">
+      <div className="relative flex h-fit w-full flex-col justify-center overflow-hidden rounded-md bg-black">
         <p className="z-10 text-5xl font-bold text-osmium sm:text-8xl">
           Osmium
         </p>
@@ -17,7 +17,7 @@ export default function Hero() {
           <br />
           <strong>Deploy</strong> smart contracts easily
         </p>
-        <div className="z-10 mt-10 flex">
+        <div className="z-10 mt-2 flex md:mt-10">
           <Link
             href="#about"
             className={buttonVariants({ variant: 'default' })}
