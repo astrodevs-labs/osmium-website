f266a1903fa85cbac88db9bdd12e95c0df49dcb2
leon3108 <maxime.noel@epitech.eu>
2024-06-21 08:32:19 +0200

    feat: add responsive for card

--- src/app/components/pricingCard.tsx
+++ src/app/components/pricingCard.tsx
@@ -3,17 +3,59 @@
 import Spline from '@splinetool/react-spline'
 import { Application } from '@splinetool/runtime'
 import { useTheme } from 'next-themes'
-import { useContext } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import { SplineContext } from 'src/app/common/splineProvider'
 
 export default function PricingCard() {
   const theme = useTheme()
   const { setSpline } = useContext(SplineContext)
+  let initialZoom: number = 0
+  const cube = useRef<any>()
+
+  function getScaleValue() {
+    const screenWidth = window.innerWidth
+    if (screenWidth > 1400) {
+      return 0.7
+    }
+    if (screenWidth > 1000) {
+      return 0.45
+    } else if (screenWidth > 700) {
+      return 0.4
+    } else if (screenWidth > 500) {
+      return 0.35
+    }
+    return 0.25
+  }
 
   function onLoad(spline: Application) {
+    const scaleValue = getScaleValue()
+    initialZoom = scaleValue
+    spline.setZoom(scaleValue)
+    const obj = spline.findObjectByName('Cube')
+    cube.current = obj
     spline.setVariable('Theme', theme.resolvedTheme!)
     setSpline(spline)
   }
+
+  function setSplineScale(scale: number) {
+    if (cube.current === undefined) return
+    const newScale = cube.current.scale
+    newScale.x = scale
+    newScale.y = scale
+    newScale.z = scale
+    cube.current.scale = newScale
+  }
+
+  useEffect(() => {
+    function scaleSpline() {
+      if (cube.current?.scale) {
+        const scaleValue = getScaleValue()
+        setSplineScale(scaleValue / initialZoom)
+      }
+    }
+    window.addEventListener('resize', scaleSpline)
+    return () => window.removeEventListener('resize', scaleSpline)
+  }, [initialZoom])
 
   return (
     <Spline
